3429043a8c52e677ddfd76621cf23f74b5500027
Yiorgos Makridakis <makridakisyiorgos@gmail.com>
2022-09-16 21:20:45 +0300

    workflow to workflows, removed google map

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -4,19 +4,18 @@ import { Banner, Divider } from "../components/common";
 
 import { personelList } from "../assets/data/";
 
-const GoogleMaps = () => {
-    const API_KEY = "AIzaSyAQ9_akuj9a1b5QHLajVLylgMnFCEXcQdw";
-
-    return (
-        <iframe
-            className="w-full "
-            width="450"
-            height="350"
-            frameBorder="0"
-            src={`https://www.google.com/maps/embed/v1/place?q=place_id:ChIJHfPPLtW_oRQRDriPCWZDP1o&key=${API_KEY}`}
-        ></iframe>
-    );
-};
+//const GoogleMaps = () => {
+//
+//    return (
+//        <iframe
+//            className="w-full "
+//            width="450"
+//            height="350"
+//            frameBorder="0"
+//            src={`https://www.google.com/maps/embed/v1/place?q=place_id:ChIJHfPPLtW_oRQRDriPCWZDP1o&key=${API_KEY}`}
+//        ></iframe>
+//    );
+//};
 
 export default function Contact(): JSX.Element {
     return (
@@ -45,15 +44,6 @@ export default function Contact(): JSX.Element {
                         ))}
                     </div>
                     <Divider />
-
-                    <div className="w-full pb-5 pl-4 text-2xl font-medium border-b border-gray-300 pt-14">
-                        {" "}
-                        Find Us{" "}
-                    </div>
-                    <div className="pt-6">
-                        <GoogleMaps />
-                    </div>
-
                     <div className="flex pt-10 pb-6 shadow-lg justify-evenly bg-gray-500/10">
                         <div className="">
                             <div className="text-xl font-medium text-center ">
@@ -83,46 +73,3 @@ export default function Contact(): JSX.Element {
         </div>
     );
 }
-
-//    return (
-//        <section className="contact-wrapper">
-//            <Banner title="Contact" />
-//            <div className="contact-main ">
-//                <div className="contact">
-//                    <h1 className="title"> Address </h1>
-//                    <div className="address">
-//                        124, Vouliagmenis Ave,
-//                        <br />
-//                        GR16674, Glyfada, Athens, Greece
-//                        <br />
-//                        <br />
-//                        <span className="bold">Phone: </span>+30 210 8982019
-//                        <br />
-//                        <span className="bold">Fax: </span>+30 210 8982474
-//                        <br />
-//                        <span className="bold">Email: </span>medcare@medcare.gr
-//                    </div>
-//                </div>
-//                <div className="personel">
-//                    <div className="personel-grid-container">
-//                        {personelList.map((personel, index) => (
-//                            <div className="personel-container" key={index}>
-//                                <h1>{personel.title}</h1>
-//                                <div className="personel-name-container">
-//                                    {personel.names.map((name, index) => (
-//                                        <div
-//                                            className="peronsel-name-item"
-//                                            key={index}
-//                                        >
-//                                            {name}
-//                                        </div>
-//                                    ))}
-//                                </div>
-//                            </div>
-//                        ))}
-//                    </div>
-//                </div>
-//            </div>
-//        </section>
-//    );
-//}
