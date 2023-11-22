b224a959e8f2c2af88f9734e78a2bf338b9fe663
Yiorgos Makridakis <makridakisyiorgos@gmail.com>
2023-11-22 17:06:09 +0200

    some minor updates

--- src/assets/data/ship-list.ts
+++ src/assets/data/ship-list.ts
@@ -1,148 +1,115 @@
 const shipList = [
-    {
-        name: "Aeolos",
-        type: "Handysize",
-        year: 2001,
-        flag: "Marshall Islands",
-        dwt: 32256,
-        images: [
-            {
-                name: "aeolos_4",
-                //inline styles for selected image
-                style: {
-                    backgroundPosition: "center 70%",
-                },
-            },
-            {
-                name: "aeolos_5",
-                //inline styles for selected image
-                style: {
-                    backgroundPosition: "center 70%",
-                },
-            },
-            {
-                name: "aeolos_2",
-                //inline styles for selected image
-                style: {
-                    backgroundPosition: "center 90%",
-                },
-            },
-            {
-                name: "aeolos_3",
-                //inline styles for selected image
-                style: {
-                    backgroundPosition: "center 90%",
-                },
-            },
-        ],
-    },
-    {
-        name: "Anthea V",
-        type: "Ultramax",
-        year: 2016,
-        flag: "Panama",
-        dwt: 66609,
-        images: [
-            {
-                name: "anthea_3",
-                //inline styles for selected image
-                style: {
-                    backgroundPosition: "center 40%",
-                },
-            },
-            {
-                name: "anthea_4",
-                //inline styles for selected image
-                style: {
-                    backgroundPosition: "center 40%",
-                },
-            },
-        ],
-    },
-    {
-        name: "Christina V",
-        type: "Ultramax",
-        year: 2018,
-        flag: "Marshall Islands",
-        dwt: 66653,
-        images: [
-            {
-                name: "christina_1",
-                //inline styles for selected image
-                style: {
-                    backgroundPosition: "center 100%",
-                },
-            },
-            {
-                name: "christina_2",
-                //inline styles for selected image
-                style: {
-                    backgroundPosition: "center 100%",
-                },
-            },
-            {
-                name: "christina_3",
-                //inline styles for selected image
-                style: {
-                    backgroundPosition: "center 100%",
-                },
-            },
-        ],
-    },
-    {
-        name: "Marylisa V",
-        type: "Supramax",
-        year: 2003,
-        flag: "Marshall Islands",
-        dwt: 52428,
-        images: [
-            {
-                name: "marylisa_1",
-                //inline styles for selected image
-                style: {
-                    backgroundPosition: "center 70%",
-                },
-            },
-            {
-                name: "marylisa_4",
-                //inline styles for selected image
-                style: {
-                    backgroundPosition: "center 100%",
-                },
-            },
-            {
-                name: "marylisa_7",
-                //inline styles for selected image
-                style: {},
-            },
-            {
-                name: "marylisa_9",
-                //inline styles for selected image
-                style: {},
-            },
-        ],
-    },
-    {
-        name: "Thrasyvoulos V",
-        type: "Ultramax",
-        year: 2016,
-        flag: "Marshall Islands",
-        dwt: 61214,
-        images: [
-            {
-                name: "thrasyvoulos_1",
-                style: {
-                    backgroundPosition: "center 100%",
-                },
-            },
-            {
-                name: "thrasyvoulos_2",
-                style: {
-                    backgroundPosition: "center 100%",
-                },
-            },
-        ],
-    },
+  {
+    name: "Aeolos",
+    type: "Handysize",
+    year: 2001,
+    flag: "Marshall Islands",
+    dwt: 32256,
+    images: [
+      {
+        name: "aeolos_4",
+        //inline styles for selected image
+        style: {
+          backgroundPosition: "center 70%",
+        },
+      },
+      {
+        name: "aeolos_5",
+        //inline styles for selected image
+        style: {
+          backgroundPosition: "center 70%",
+        },
+      },
+      {
+        name: "aeolos_2",
+        //inline styles for selected image
+        style: {
+          backgroundPosition: "center 90%",
+        },
+      },
+      {
+        name: "aeolos_3",
+        //inline styles for selected image
+        style: {
+          backgroundPosition: "center 90%",
+        },
+      },
+    ],
+  },
+  {
+    name: "Anthea V",
+    type: "Ultramax",
+    year: 2016,
+    flag: "Panama",
+    dwt: 66609,
+    images: [
+      {
+        name: "anthea_3",
+        //inline styles for selected image
+        style: {
+          backgroundPosition: "center 40%",
+        },
+      },
+      {
+        name: "anthea_4",
+        //inline styles for selected image
+        style: {
+          backgroundPosition: "center 40%",
+        },
+      },
+    ],
+  },
+  {
+    name: "Christina V",
+    type: "Ultramax",
+    year: 2018,
+    flag: "Marshall Islands",
+    dwt: 66653,
+    images: [
+      {
+        name: "christina_1",
+        //inline styles for selected image
+        style: {
+          backgroundPosition: "center 100%",
+        },
+      },
+      {
+        name: "christina_2",
+        //inline styles for selected image
+        style: {
+          backgroundPosition: "center 100%",
+        },
+      },
+      {
+        name: "christina_3",
+        //inline styles for selected image
+        style: {
+          backgroundPosition: "center 100%",
+        },
+      },
+    ],
+  },
+  {
+    name: "Thrasyvoulos V",
+    type: "Ultramax",
+    year: 2016,
+    flag: "Marshall Islands",
+    dwt: 61214,
+    images: [
+      {
+        name: "thrasyvoulos_1",
+        style: {
+          backgroundPosition: "center 100%",
+        },
+      },
+      {
+        name: "thrasyvoulos_2",
+        style: {
+          backgroundPosition: "center 100%",
+        },
+      },
+    ],
+  },
 ];
 
 export default shipList;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,99 +5,95 @@ import { Carousel, HomeShipBox, Divider } from "../components/common";
 import { sliderImageList } from "../assets/data";
 
 const shipBoxData = [
-    {
-        name: "Aeolos",
-        image: "aeolos_2",
-    },
-    {
-        name: "Anthea V",
-        image: "anthea_1",
-    },
-    {
-        name: "Christina V",
-        image: "christina_1",
-    },
-    {
-        name: "Marylisa V",
-        image: "marylisa_1",
-    },
-    {
-        name: "Thrasyvoulos V",
-        image: "thrasyvoulos_1",
-    },
+  {
+    name: "Aeolos",
+    image: "aeolos_2",
+  },
+  {
+    name: "Anthea V",
+    image: "anthea_1",
+  },
+  {
+    name: "Christina V",
+    image: "christina_1",
+  },
+  {
+    name: "Thrasyvoulos V",
+    image: "thrasyvoulos_1",
+  },
 ];
 
 export default function Home(): JSX.Element {
-    const [serviceImg, setServiceImg] = useState<string>();
-    const [moreImg, setMoreImg] = useState<string>();
+  const [serviceImg, setServiceImg] = useState<string>();
+  const [moreImg, setMoreImg] = useState<string>();
 
-    import("../assets/images/anthea_3.jpg").then((img) => {
-        setServiceImg(img.default);
-    });
-    import("../assets/images/aeolos_4.jpg").then((img) => {
-        setMoreImg(img.default);
-    });
-    return (
-        <section className="mt-[100px]">
-            <Carousel images={sliderImageList} />
+  import("../assets/images/anthea_3.jpg").then((img) => {
+    setServiceImg(img.default);
+  });
+  import("../assets/images/aeolos_4.jpg").then((img) => {
+    setMoreImg(img.default);
+  });
+  return (
+    <section className="mt-[100px]">
+      <Carousel images={sliderImageList} />
 
-            <div className="container mx-auto mt-2">
-                <Divider />
-                <h1 className="py-5 text-2xl font-medium tracking-widest text-center ">
-                    {" "}
-                    About
-                </h1>
-                <p className="mx-auto text-sm text-center">
-                    Alexandria Shipping (Hellas) S.A. is a Shipping Company
-                    operating Bulk Carriers.
-                    <br /> The company supports the need for a comprehensive and
-                    effective Quality & Environmental management system to
-                    ensure that the services offered by this organization meet
-                    customer requirements
-                </p>
-                <Divider />
-            </div>
-            <div
-                className="w-full h-[400px] my-10 bg-center max-h-full bg-no-repeat bg-auto md:bg-cover "
-                style={{ backgroundImage: `url(${serviceImg})` }}
-            ></div>
-            <div className="container mx-auto mt-2">
-                <Divider />
-                <h1 className="py-5 text-2xl font-medium tracking-widest text-center">
-                    {" "}
-                    Services
-                </h1>
-                <p className="pb-4 mx-auto text-sm text-center">
-                    Alexandria Shipping (Hellas) S.A. management services
-                    include operating, maintaining, crewing, and trading the
-                    vessels worldwide in a safe, efficient, economic and
-                    profitable way. <br />
-                    The Management’s headquarters are located in Omonia, Athens,
-                    Greece
-                </p>
-                <Divider />
-            </div>
-            <div className="container mx-auto mt-2"></div>
-            <div
-                className="w-full h-[400px] my-10 bg-center max-h-full bg-no-repeat bg-auto md:bg-cover "
-                style={{ backgroundImage: `url(${moreImg})` }}
-            ></div>
+      <div className="container mx-auto mt-2">
+        <Divider />
+        <h1 className="py-5 text-2xl font-medium tracking-widest text-center ">
+          {" "}
+          About
+        </h1>
+        <p className="mx-auto text-sm text-center">
+          Alexandria Shipping (Hellas) S.A. is a Shipping Company
+          operating Bulk Carriers.
+          <br /> The company supports the need for a comprehensive and
+          effective Quality & Environmental management system to
+          ensure that the services offered by this organization meet
+          customer requirements
+        </p>
+        <Divider />
+      </div>
+      <div
+        className="w-full h-[400px] my-10 bg-center max-h-full bg-no-repeat bg-auto md:bg-cover "
+        style={{ backgroundImage: `url(${serviceImg})` }}
+      ></div>
+      <div className="container mx-auto mt-2">
+        <Divider />
+        <h1 className="py-5 text-2xl font-medium tracking-widest text-center">
+          {" "}
+          Services
+        </h1>
+        <p className="pb-4 mx-auto text-sm text-center">
+          Alexandria Shipping (Hellas) S.A. management services
+          include operating, maintaining, crewing, and trading the
+          vessels worldwide in a safe, efficient, economic and
+          profitable way. <br />
+          The Management’s headquarters are located in Omonia, Athens,
+          Greece
+        </p>
+        <Divider />
+      </div>
+      <div className="container mx-auto mt-2"></div>
+      <div
+        className="w-full h-[400px] my-10 bg-center max-h-full bg-no-repeat bg-auto md:bg-cover "
+        style={{ backgroundImage: `url(${moreImg})` }}
+      ></div>
 
-            <Divider />
-            <HomeShipBox data={shipBoxData} />
+      <Divider />
+      <HomeShipBox data={shipBoxData} />
 
-            <div className="container mx-auto my-10">
-                <Divider />
-                <h1 className="py-5 text-2xl font-medium tracking-widest text-center">
-                    {" "}
-                    More Info{" "}
-                </h1>
-                <p className="mx-auto mb-10 text-sm text-center">
-                    The Company Management System (CMS) is designed to operate
-                    within the requirements of ISO 9001:2008, ISO 14001:2004,
-                    the ISM code.
-                </p>
-            </div>
-        </section>
-    );
+      <div className="container mx-auto my-10">
+        <Divider />
+        <h1 className="py-5 text-2xl font-medium tracking-widest text-center">
+          {" "}
+          More Info{" "}
+        </h1>
+        <p className="mx-auto mb-10 text-sm text-center">
+          The Company Management System (CMS) is designed to operate
+          within the requirements of ISO 9001:2008, ISO 14001:2004,
+          the ISM code.
+        </p>
+      </div>
+    </section>
+  );
 }
